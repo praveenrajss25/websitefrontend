c243872b831bc353dd67db07040ff8259ae59cd9
praveenrajss25 <praveenrajsamaraji2005@gmail.com>
2025-05-31 20:44:11 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,5 @@
+const hamburger=document.querySelector('.hamburger')
+const navbar= document.querySelector('ul');
+hamburger.addEventListener('click',()=>{
+    navbar.classList.toggle('slide');
+});
